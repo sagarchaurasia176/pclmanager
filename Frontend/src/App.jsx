96de79a6a045e659085bac9e73cba6c8fc78668b
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-06-12 19:27:30 +0530

    half work done in login auth

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import LoginSlice from "./redux/Slice.js/LoginSlice";
+import LoginAuth from "./auth/LoginAuth";
+import {Routes,Route} from 'react-router-dom'
+import StudentLogin from "./auth/StudentLogin";
+import TeacherLogin from "./auth/TeacherLogin";
+
 function App() {
   return(
     <> 
-      
+    <Routes>
+      <Route path="/" element={<LoginAuth/>}></Route>
+      <Route path="/TeacherLogin" element={<TeacherLogin/>}></Route>
+      <Route path="/StudentLogin" element={<StudentLogin/>}></Route>
+  
+    </Routes>
+  
+
     </>
   );
 }
