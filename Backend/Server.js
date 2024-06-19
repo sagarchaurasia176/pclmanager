844de2a81d69585216870c30ccd3298a5b945b4d
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-06-19 15:19:11 +0530

    code upadted in the redux page

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,18 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
-
 // listen port apply here
 app.listen(PORT, () => {
-  console.log(`server is running at port" ${ PORT }`);
+  console.log(`server is running at port" ${PORT}`);
 });
-
 // passed the middlware here so we get
 app.use(express.json());
-
 // call the db here by requires => and connnect with the userAuthDbConnectioon();
 require("./config/DbConnection").userAuthDbConnectionSetup();
-    
 // routes import and mount here
-const mainManagerRoutes = require("./routes/MainManagerRoutes");
-app.use("/mainManagerRoutes", mainManagerRoutes);
+// const routes = require("./routes/MainManagerRoutes");
+// app.use("/mainManagerRoutes", routes);
