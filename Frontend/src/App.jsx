c0cac90016927d31862e04d060f500d8453b39ba
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-06-22 01:26:54 +0530

    minor change in the ui part

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,12 @@
-// App.jsx
-
 import React from "react";
 import "./App.css";
-import LoginAuth from "./auth/LoginAuth";
+import LoginAuth from "./auth/log/LoginAuth";
 import { Routes, Route } from "react-router-dom";
 import GuideDashboard from "./Dashboard/Guide/GuideDashboard";
 import MainAdmin from "./Dashboard/Admin/AdminDashboard";
 import StudentDashboard from "./Dashboard/Student/StudentDashboard";
-import StudentLogin from "./auth/StudentLogin";
-import RegisterStudentForm from "./auth/RegisterStudentForm";
+// import StudentLogin from "./auth/log/StudentLogin";
+import RegisterStudentForm from "./auth/constant/RegisterStudentForm";
 function App() {
   return (
     <>
