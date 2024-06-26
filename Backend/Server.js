2e31dea0a92cc695472f2a5b9023342e361eda9c
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-06-27 02:20:06 +0530

    docker

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 // connect with the postgrees db
 app.use(express.json());
 // call the db postgress here
-// const {sequelize , testDbConnection} = require('./config/DbConnection')
-// testDbConnection();
-
-
+// port applied here
+const port = process.env.PORT_NUM || 8000;
+// listen at this ports
+app.listen(port, () => {
+  console.log(`"server running at this port" ${port}`);
+});
+// app.get apply here
+app.get("/", (req, res) => {
+  res.send("backend running find");
+});
