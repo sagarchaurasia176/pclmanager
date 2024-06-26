37ebf248bed64bd6bfd76261e2efee3095971e44
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-06-26 14:52:10 +0530

    install sequlize , connect postgres db wtih the docker

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,7 +3,7 @@ const app = express();
 // connect with the postgrees db
 app.use(express.json());
 // call the db postgress here
-const {sequelize , testDbConnection} = require('./config/DbConnection')
-testDbConnection();
+// const {sequelize , testDbConnection} = require('./config/DbConnection')
+// testDbConnection();
 
 
