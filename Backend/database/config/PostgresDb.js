e3bc021867e430f127392820f417cfffb5b1c3a6
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-06-28 11:24:48 +0530

    config

---
+++ Backend/database/config/PostgresDb.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+}
+);
+
+// Test the database connection
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+// db connections clalls
+
+testConnection();
+
+module.exports = sequelize;
