af7d8aa55293f42edf94915014aa5c2b8c64c456
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-06-29 04:05:06 +0530

    work in backend side

--- Backend/config/DbConnection.js
+++ Backend/config/DbConnection.js
@@ -1,2 +1,14 @@
 // mongo db itegration
-const mongoose
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbConnection = () => {
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+  }).then(()=>{
+    console.log("db connected succesfully....")
+  }).catch((er)=>{
+    console.log("error in db connection" , er);
+  });
+};
+module.exports = dbConnection;
