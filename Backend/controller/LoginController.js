244cb12f42e4778b2754436e6a43e9f0daf20ae2
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-06-29 13:10:08 +0530

    backend code updated

---
+++ Backend/controller/LoginController.js
@@ -0,0 +1,68 @@
+const PCLFormSchema = require("../model/PCLFormSchema");
+// login auth apply
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+// login controller apply here
+exports.LoginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // fetch the data from the useres
+    //validation on email and password
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "PLease fill all the details carefully",
+      });
+    }
+    let emailExisting = await PCLFormSchema.findOne({ email: email });
+    //if not a registered user
+    if (!emailExisting) {
+      return res.status(401).json({
+        success: false,
+        message: "your email is not registered",
+      });
+    }
+
+    // jwt with cokkies
+    const payload = {
+      id: emailExisting._id,
+      email: emailExisting.email,
+    };
+    const options = {
+      expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+    // here jwt create
+    if (await bcrypt.compare(password, emailExisting.password)) {
+      let jwtToken = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+
+      emailExisting = emailExisting.toObject();
+      // password set to be hide from the users
+      emailExisting.jwtToken = emailExisting;
+      emailExisting.password = undefined;
+
+      // set the cookies here
+      res.cookie("login", jwtToken, options).status(200).json({
+        success: true,
+        jwtToken,
+        emailExisting,
+        message: "User Logged in successfully",
+      });
+    } else {
+      //passwsord do not match
+      return res.status(403).json({
+        success: false,
+        message: "Password Incorrect",
+      });
+    }
+  } catch (er) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error !",
+      Error: er.message,
+    });
+  }
+};
