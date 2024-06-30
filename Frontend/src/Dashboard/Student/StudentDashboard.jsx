8bb8eb2cd71f7bc997196da0832092d68e03126b
Gunal <gunalsoftware@gmail.com>
2024-06-30 10:02:51 +0530

    add student dashboard

--- Frontend/src/Dashboard/Student/StudentDashboard.jsx
+++ Frontend/src/Dashboard/Student/StudentDashboard.jsx
@@ -1,8 +1,17 @@
-import React, {useState} from 'react'
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+
+import { GiTeamIdea } from "react-icons/gi";
+import { MdDoubleArrow } from "react-icons/md";
+import { MdCastForEducation } from "react-icons/md";
+import { ImUserCheck } from "react-icons/im";
+
+
+
 
 const StudentDashboard = () => {
-    const [activeLink, setActiveLink] = useState('');
+  const [activeLink, setActiveLink] = useState("");
 
   const handleLinkClick = (link) => {
     setActiveLink(link);
@@ -15,17 +24,23 @@ const StudentDashboard = () => {
         {/* Navbar */}
         <div className="navbar bg-base-300 w-full">
           <div className="flex-none lg:hidden">
-            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                className="inline-block h-6 w-6 stroke-current">
+                className="inline-block h-6 w-6 stroke-current"
+              >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"></path>
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
               </svg>
             </label>
           </div>
@@ -33,19 +48,23 @@ const StudentDashboard = () => {
           <div className="hidden flex-none lg:block">
             <ul className="menu menu-horizontal">
               <li>
-                <NavLink 
-                  to="/student/ViewTeam" 
-                  className={`nav-link ${activeLink === '/student/ViewTeam' ? 'active' : ''}`} 
-                  onClick={() => handleLinkClick('/student/ViewTeam')}
+                <NavLink
+                  to="/student/ViewTeam"
+                  className={`nav-link ${
+                    activeLink === "/student/ViewTeam" ? "active" : ""
+                  }`}
+                  onClick={() => handleLinkClick("/student/ViewTeam")}
                 >
                   View Team
                 </NavLink>
               </li>
               <li>
-                <NavLink 
-                  to="/student/ViewMarkAtt" 
-                  className={`nav-link ${activeLink === '/student/ViewMarkAtt' ? 'active' : ''}`} 
-                  onClick={() => handleLinkClick('/student/ViewMarkAtt')}
+                <NavLink
+                  to="/student/ViewMarkAtt"
+                  className={`nav-link ${
+                    activeLink === "/student/ViewMarkAtt" ? "active" : ""
+                  }`}
+                  onClick={() => handleLinkClick("/student/ViewMarkAtt")}
                 >
                   Attendance / Marks
                 </NavLink>
@@ -54,22 +73,63 @@ const StudentDashboard = () => {
           </div>
         </div>
         {/* Page content here */}
-        <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
+
+
+
+        <div class="flex flex-row hero bg-base-200 min-h-screen">
+
+          <div class="basis w-full m-10">
+            <div className="card bg-primary text-primary-content">
+              <div className="card-body">
+                <div>
+                  <GiTeamIdea className="object-cover w-16 h-16"/> 
+                </div>
+                <h2 className="card-title">View Teams</h2>
+                
+                <div className="card-actions justify-end">
+                  <button className="btn">Click <MdDoubleArrow /> </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="basis w-full m-10">
+            <div className="card bg-primary text-primary-content">
+              <div className="card-body">
+                <div>
+                  <MdCastForEducation className="object-cover w-16 h-16"/> 
+                </div>
+                <h2 className="card-title">Student Marks</h2>
+                <div className="card-actions justify-end">
+                  <button className="btn">Click <MdDoubleArrow /> </button>
+                </div>
+              </div> 
+            </div>
+          </div>
+
+          <div class="basis w-full m-10">
+            <div className="card bg-primary text-primary-content">
+              <div className="card-body">
+                <div>
+                  <ImUserCheck className="object-cover w-16 h-16 rounded-full"/> 
+                </div>
+                <h2 className="card-title">Attedance</h2>
+                
+                <div className="card-actions justify-end">
+                  <button className="btn">Click <MdDoubleArrow /> </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
         
+
+
       </div>
     </div>
   );
-}
+};
 
-export default StudentDashboard
+export default StudentDashboard;
